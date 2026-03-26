7d4791d390c2b07d3e0008243e66cd5f8ce00e82
khanh tran <khanh.tran.vinh@gmail.com>
2026-03-25 20:51:51 -0400

    Initial Commit

---
+++ NomLens/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NomLens
+//
+//  Created by kt on 2026-03-25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NomLensTests/NomLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  NomLensTests.swift
+//  NomLensTests
+//
+//  Created by kt on 2026-03-25.
+//
+
+import Testing
+@testable import NomLens
+
+struct NomLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
